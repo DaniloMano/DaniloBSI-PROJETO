e21de7c96083cbc77858b590c39447ca88ad7aa6
DaniloMano <danilogabrill@outlook.com>
2023-08-19 20:51:30 -0300

    Adicionei a tela inicial fora da função "main"

--- Projeto-Rede-Danilo.c
+++ Projeto-Rede-Danilo.c
@@ -1,9 +1,43 @@
+//===============================================================================
+//===               Universidade Federal do Rio Grande do Norte               ===
+//===============================================================================
+//===                   Centro de Ensino Superior do Seridó                   ===
+//===                Departamento de Computação e Tecnologia                  ===
+//===                   Disciplina DCT1106 -- Programação                     ===
+//===-------------------------------------------------------------------------===
+//===      Projeto Sistema de Gestão para uma Fábrica de Redes de Dormir      ===
+//===============================================================================
+//===             Developed by @DaniloMano -- since Aug, 2023                 ===
+//===============================================================================
+
 #include <stdio.h>
 #include <stdlib.h>
-int main()
+#include <locale.h>
+
+void tela_inicial(void)
 {
+
+    setlocale(LC_ALL,"Portuguese_Brazil");
+
     printf("===============================================================================");
     printf("\n");
-    printf("Sistema de GestÃ£o para uma FÃ¡brica de Redes de Dormir");
-    return 0;
+    printf("===               Universidade Federal do Rio Grande do Norte               ===");
+    printf("\n");
+    printf("===============================================================================");
+    printf("\n");
+    printf("===                   Centro de Ensino Superior do Seridó                   ===");
+    printf("\n");
+    printf("===                Departamento de Computação e Tecnologia                  ===");
+    printf("\n");
+    printf("===                   Disciplina DCT1106 -- Programação                     ===");
+    printf("\n");
+    printf("===-------------------------------------------------------------------------===");
+    printf("\n");
+    printf("===      Projeto Sistema de Gestão para uma Fábrica de Redes de Dormir      ===");
+    printf("\n");
+    printf("===============================================================================");
+    printf("\n");
+    printf("===             Developed by @DaniloMano -- since Aug, 2023                 ===");
+    printf("\n");
+    printf("===============================================================================");
 }
